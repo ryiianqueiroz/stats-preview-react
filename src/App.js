244fe6af2ef8ca26b0c58281a4bcf367cefeabb7
ryan <ryanqueirozdev44@gmail.com>
2023-10-06 11:11:10 -0300

    Subindo Projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import HeaderDesktop from './images/image-header-desktop.jpg'
+import HeaderMobile from './images/image-header-mobile.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="content">
+          <div className="text">
+            <h1>Get <span className='insights'>insights</span> that help your business grow.</h1>
+            <p id='p-main'>Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+            experience, and overall efficiency.</p>
+
+            <div className="cards">
+              <div className="card">
+                <h3>10k+</h3>
+                <p>companies</p>
+              </div>
+              <div className="card">
+                <h3>314</h3>
+                <p>templates</p>
+              </div>
+              <div className="card">
+                <h3>12m+</h3>
+                <p>queries</p>
+              </div>
+            </div>
+          </div>
+          <div className="image">
+            <img src={HeaderDesktop} alt="#" id='header-image'/>
+            <img src={HeaderMobile} alt="#" id='header-mobile'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
